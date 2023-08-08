b1cfaea42b66f0ff6faefa471606544f2d24483a
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-08-09 00:16:27 +0100

    added advanced

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-remove_char_at = __import__('101-remove_char_at').remove_char_at
+def remove_char_at(str, n):
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
