7ce4b30579b8101c241e90910f0d98bbe06e24d5
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-08-11 18:31:23 +0100

    modified 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from add_o import add
 
-a = 1
-b = 2
+if __name__ == "__main__":
+    """Print the sum of two numbers"""
+    from add_0 import add
 
-if __name__ == '__main__':
-	print(f"{a} + {b} = {add(a, b)}")
+    a = 1
+    b = 2
+    print("{a} + {b} = {sum}".format(a=a, b=b, sum=add(a, b)))
